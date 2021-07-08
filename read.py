e61c728308f8ace85525beabc69d84aee775fdde
unknown <darrius.clk@gmail.com>
2021-07-08 11:52:30 +0800

    modified can cal average len

--- read.py
+++ read.py
@@ -6,6 +6,8 @@ with open('reviews.txt', 'r') as f:
 		count += 1
 		if count % 100000 == 0:
 			print(len(data))
-
-print(len(data))
-print(data[100])
+print('total no. of reviews',len(data))
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+print('average length', sum_len / len(data))
